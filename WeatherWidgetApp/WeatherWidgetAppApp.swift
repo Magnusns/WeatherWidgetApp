a0be81592254ea6ad80a7cd9ca444a7b4e8f47b5
Magnusns <soedergren.97@gmail.com>
2025-04-25 16:01:08 +0200

    Initial Commit

---
+++ WeatherWidgetApp/WeatherWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetAppApp.swift
+//  WeatherWidgetApp
+//
+//  Created by Magnus Nilsen Sødergren on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
